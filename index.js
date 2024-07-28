b1b4ff385fe9e0cf63474699d71312bc27199fda
TNT <139092950+TNTxTrick@users.noreply.github.com>
2024-07-28 01:22:33 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Serve HTML pages
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
+app.get('/login', (req, res) => res.sendFile(path.join(__dirname, 'public/login.html')));
+app.get('/register', (req, res) => res.sendFile(path.join(__dirname, 'public/register.html')));
+
+// API route
+app.get('/api', (req, res) => {
+    res.json({ message: 'This is the API response' });
+});
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
